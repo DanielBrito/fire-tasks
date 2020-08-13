363babe1b8690518e9e4719cf91620e000b78019
Daniel <daniell-henrique@hotmail.com>
2020-08-12 23:33:32 -0300

    Template firebase.js

--- fire-tasks-app/src/config/firebase.js
+++ fire-tasks-app/src/config/firebase.js
@@ -1,19 +1,18 @@
 import firebase from "firebase/app";
-import "firebase/firestore";
+import "firebase/firebase-firestore";
 
-// Set your app configuration:
 var firebaseConfig = {
-  apiKey: "...",
-  authDomain: "...",
-  databaseURL: "...",
-  projectId: "...",
-  storageBucket: "...",
-  messagingSenderId: "...",
-  appId: "...",
+  apiKey: "AIzaSyD6o-nJj5qiDi1nVzIYenDntCVfsGkEyx8",
+  authDomain: "fire-tasks-a3674.firebaseapp.com",
+  databaseURL: "https://fire-tasks-a3674.firebaseio.com",
+  projectId: "fire-tasks-a3674",
+  storageBucket: "fire-tasks-a3674.appspot.com",
+  messagingSenderId: "949414732563",
+  appId: "1:949414732563:web:74c800b643ada93f1970e6",
 };
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-const databaseRef = firebase.database().ref();
+const databaseRef = firebase.firestore();
 
-export const tasksRef = databaseRef.child("tasks");
+export const tasksRef = databaseRef.collection("tasks");
